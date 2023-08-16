663472b6b3e8c5706d2d9a58f923b8128b9ec53b
Shivam Purohit <codesp07@gmail.com>
2023-08-17 01:01:13 +0530

    chore: Get and Set operations on singlyLinkedList

---
+++ SinglyLinkedLists/Singly_Linked_List_Get.js
@@ -0,0 +1,99 @@
+class Node{
+    constructor(val){
+        this.data = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(head,tail){
+        this.head = head;
+        this.tail = tail;
+        this.length = 0;
+    }
+
+    get(idx){
+        /* Get the node at the mentioned index */
+        if(this.length <= idx || idx < 0){
+            return null;
+        }
+       
+            var currentNode = this.head;
+            var currentIndex = 0;
+            while(currentIndex !== idx){
+                currentNode = currentNode.next;
+                currentIndex++;
+            }
+
+       return currentNode;
+    }
+
+    unshift(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    shift(){
+       /* Remove the element from the begining */
+       if(!this.head)
+        return null;
+       
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+       
+       return currentHead;
+    }
+
+    push(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    pop(){
+        if(!this.head) return null;
+        var current = this.head;
+        var newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+
+}
+
+const list = new SinglyLinkedList();
+list.push('Text1');
+list.push('Text2');
+list.push('Text3');
+list.push('Text4');
+console.log('list push',list)
+
+const node2 = list.get(2);
+console.log('list get',node2)
